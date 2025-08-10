63da2fe07e4005599c1072c3d64b56ec9d42c794
adityatheperson <44348836+adityatheperson@users.noreply.github.com>
2025-08-09 21:34:15 -0700

    a

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { screentime } from 'screentime';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    screentime.echo({ value: inputValue })
-}
--- src/definitions.ts
+++
@@ -1,5 +0,0 @@
-export interface ScreenTimePlugin {
-    enableScreenTimeAccess(): Promise<void>;
-    setAppLimit(appIdentifier: string, limit: number): Promise<void>;
-    getAppUsage(appIdentifier: string): Promise<{ usage: number }>;
-}
